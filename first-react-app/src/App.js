e2ed10a0da53c84a1786647460dfd2f7cd09682f
Yash Garg <thisisyashgarg@gmail.com>
2022-11-19 16:18:00 +0530

    creating components in react, jsx syntax

---
+++ first-react-app/src/App.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const helloWorld = <h1>HELLO WORLD!!</h1>;
--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom'
-// import React from 'react'
+import { helloWorld } from './App.js';
 
-const helloWorld = <h1>HELLO WORLD!</h1>
+
 const rootElement = document.getElementById('root'); //jsx, way to create component in react
 
+// const helloWorld = <h1>HELLO WORLD!!</h1>
+// const helloWorld2 = React.createElement('h1', null, 'Hello pesto');
+//helloWorld translates into helloWorld2 in inside react, but helloWorld2 
+//is easy to make
+console.log(typeof helloWorld);
 ReactDOM.render(helloWorld, rootElement);
