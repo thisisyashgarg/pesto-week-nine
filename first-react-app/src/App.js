a69b650614881218d3f462b016df87ed828dc84c
Yash Garg <thisisyashgarg@gmail.com>
2022-11-20 21:19:59 +0530

    learned about function and class components, props, fragments

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,3 +1,30 @@
 import React from "react";
+import ReactDOM from 'react-dom'
 
-export const helloWorld = <h1>HELLO WORLD!!</h1>;
+//one way to create component
+// const helloWorld = <h1>HELLO WORLD again!!</h1>;
+
+
+//Class component
+class classComponent extends React.Component{
+     render(){
+        const {name} = this.props;
+        return (
+            <>
+            <h1>I am a class component with name {name} </h1>
+            <button onClick={function(){console.log('clicked')}}>Click Me</button>
+            </>
+        );
+     }   
+}
+// <> and </> are called fragments
+
+
+//function component
+function functionComponent(){
+    
+    return (<h1>I am a function component with name </h1>);
+    
+}
+
+export { classComponent, functionComponent }; //exporting class
--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,12 +1,37 @@
 import ReactDOM from 'react-dom'
-import { helloWorld } from './App.js';
+import React from 'react';
+// import {classComponent, functionComponent } from './App.js';
 
 
-const rootElement = document.getElementById('root'); //jsx, way to create component in react
+const rootElement = document.getElementById('root'); 
 
-// const helloWorld = <h1>HELLO WORLD!!</h1>
+//normal jsx
+// const helloWorld = <h1>HELLO WORLD!!</h1> 
 // const helloWorld2 = React.createElement('h1', null, 'Hello pesto');
 //helloWorld translates into helloWorld2 in inside react, but helloWorld2 
 //is easy to make
-console.log(typeof helloWorld);
-ReactDOM.render(helloWorld, rootElement);
+
+
+//class component 
+// const input = new classComponent();
+// ReactDOM.render( input.render(), rootElement);
+function functionComponent(){
+    return (<h1>I am a function component with name </h1>); 
+}
+
+//function component
+ReactDOM.render( <this.functionComponent/>, rootElement);
+
+
+//composing components
+// let compose = 
+// <div>
+//     <functionComponent name = "yash"/>
+//     <functionComponent name = "ankit"/>
+//     <functionComponent name = "rohan"/>
+// </div>
+
+// ReactDOM.render( compose, rootElement);
+
+
+
